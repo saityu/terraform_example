ea7546035c60b3303c436558d1720fb9194a7322
saityu <s.yusuke.dev@gmail.com>
2020-05-18 00:05:31 +0900

    5-5.2

--- main.tf
+++ main.tf
@@ -2,11 +2,9 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-module "web_server" {
-  source        = "./http_server"
-  instance_type = "t3.micro"
-}
-
-output "public_dns" {
-  value = module.web_server.public_dns
+module "describe_regitons_for_ec2" {
+  source     = "./iam_role"
+  name       = "describe-regitons-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy     = var.policy
 }
