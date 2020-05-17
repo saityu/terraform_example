d42fed79fea8c2df14d52754ff4d710ac09363e1
saityu <s.yusuke.dev@gmail.com>
2020-05-17 22:40:24 +0900

    3-3.7

--- main.tf
+++ main.tf
@@ -40,18 +40,14 @@ resource "aws_security_group" "example_ec2" {
 }
 
 resource "aws_instance" "example" {
-  ami           = data.aws_ami.recent_amazon_linux_2.image_id
-  instance_type = local.example_instance_type
+  ami                    = data.aws_ami.recent_amazon_linux_2.image_id
+  instance_type          = local.example_instance_type
   vpc_security_group_ids = [aws_security_group.example_ec2.id]
   tags = {
     Name = "example"
   }
 
-  user_data = <<EOF
-    #!bin/bash
-    yum install -y httpd
-    systemctl start httpd.service
-  EOF
+  user_data = file("./user_data.sh")
 }
 
 output "example_instance_id" {
